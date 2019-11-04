0af86c506c2624ec9153917d823ccb05078f738e
Wilson Gaturu <wilsongaturu@gmail.com>
2019-11-04 11:31:40 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactMapGL, {SVGOverlay} from 'react-map-gl';
+
+function hoverHandler(e) {
+  console.log(e.lngLat);
+}
+
+function redraw(obj) {
+  console.log(this);
+  // const [cx, cy] = project([-122, 37]);
+  // return <circle cx={cx} cy={cy} r={40} fill="blue" />;
+}
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 1.286386,
+    longitude: 36.817223,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ReactMapGL 
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={viewport => {setViewport(viewport)}}
+        onClick={hoverHandler}
+        mapStyle="mapbox://styles/actlikewill/ck2g3scvn0pjs1cpg5r7xshds"
         >
-          Learn React
-        </a>
-      </header>
+        <SVGOverlay redraw={redraw} />
+      </ReactMapGL>
+      <div>
+        hellow
+      </div>
     </div>
   );
 }
