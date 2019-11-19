955e3af7c262b1aa8d47164eaf9d54b55b006752
actlikewill <wilsongaturu@gmail.com>
2019-11-19 16:47:48 +0300

    ui layout

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import turf from 'turf';
+import trashIcon from './img/garbage.svg';
+import boxIcon from './img/box.svg';
+import circleIcon from './img/circle.svg';
+import polyIcon from './img/polygon.svg';
 
-
-//  TODO: set page title
 
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 let map;
 
+// TODO: change document title
 
 class Map extends Component {
     constructor(props) {
@@ -35,23 +38,28 @@ class Map extends Component {
             });        
     }
 
-    setDrawingMode = (type) => {
-        this.setState({ type });
+    setDrawingMode = (type) => {        
+        this.setState({  type });
         map.on('click', this.setCoordinates);
     }
     
-    clickHandler = ({ target: { id } }) => {
-        this.setDrawingMode(id);
+    clickHandler = ({ currentTarget: { id } }) => {
+        const { type } = this.state;
+        if(!type) {
+            this.setDrawingMode(id);
+        } else {
+            this[type]();
+        }               
     }
 
     plot = (coordinates) => {        
         if(!map.getLayer('plotLayer')) {
-            this.draw('MultiPoint', coordinates, 'plotLayer', 'circle', {'circle-color': '#888',
+            this.draw('MultiPoint', coordinates, 'plotLayer', 'circle', {'circle-color': '#002080',
             'circle-radius': 5});       
         } else {
             map.removeLayer('plotLayer');
             map.removeSource('plotLayer');
-            this.draw('MultiPoint', coordinates, 'plotLayer', 'circle', {'circle-color': '#888',
+            this.draw('MultiPoint', coordinates, 'plotLayer', 'circle', {'circle-color': '#002080',
             'circle-radius': 5});
         }       
     } 
@@ -67,7 +75,7 @@ class Map extends Component {
         this.plot(this.state.plotCoordinates);             
     }
 
-    closePolygon = () => {        
+    Polygon = () => {        
         const { plotCoordinates, type } = this.state;        
         if (plotCoordinates.length > 2 && type === 'Polygon' && !map.getLayer('polygonLayer')) {
             const firstPoint = plotCoordinates[0];
@@ -79,29 +87,31 @@ class Map extends Component {
                     polygonCoordinates,
                     'polygonLayer',
                     'fill',
-                    {   'fill-color': '#ddd' }
+                    { 'fill-color': '#3366ff', 'fill-opacity': .5, 'fill-outline-color': '#002080' }
                     );
-            });            
+            });             
             map.off('click', this.setCoordinates);    
             }   
         }
       
-     closeCircle = () => {
+     Circle = () => {
          const { plotCoordinates, type } = this.state;    
-         if(plotCoordinates.length === 2 && type === 'Point' && !map.getLayer('circleLayer')) {
-            const distance = turf.distance(plotCoordinates[0], plotCoordinates[1], 'kilometers') * 1000;
+         if(plotCoordinates.length === 2 && type === 'Circle' && !map.getLayer('circleLayer')) {
+            const radius = turf.distance(plotCoordinates[0], plotCoordinates[1], 'kilometers') * 1000;
             const metersToPixelsAtMaxZoom = (meters, latitude) => meters / 0.075 / Math.cos(latitude * Math.PI / 180);
-            const circleCoordinates = { center: plotCoordinates[0], distance };
+            const circleCoordinates = { center: plotCoordinates[0], radius };
             this.setState({ circleCoordinates }, () => {
                 const { circleCoordinates } = this.state;
                 this.draw(
-                    type,
+                    'Point',
                     circleCoordinates.center,
                     'circleLayer',
                     'circle',
-                    {   'circle-color':'#ddd',
+                    {   'circle-color':'#3366ff',
+                        'circle-opacity': .5,
+                        'circle-stroke-color': '#002080',  
                         'circle-radius': {
-                                            stops: [[0, 0], [20, metersToPixelsAtMaxZoom(circleCoordinates.distance, circleCoordinates.center[1])]], base: 2
+                                            stops: [[0, 0], [20, metersToPixelsAtMaxZoom(circleCoordinates.radius, circleCoordinates.center[1])]], base: 2
                                         }
                     }
                 );
@@ -110,9 +120,9 @@ class Map extends Component {
          map.off('click', this.setCoordinates); 
      }
 
-     closeBoundingBox = () => {
+     BoundingBox = () => {
          const { plotCoordinates, type } = this.state;  
-         if(plotCoordinates.length === 2 && type === 'boundingBox' && !map.getLayer('boundingBoxLayer')) {  
+         if(plotCoordinates.length === 2 && type === 'BoundingBox' && !map.getLayer('boundingBoxLayer')) {  
             const mirroredPoint1 = [plotCoordinates[1][0], plotCoordinates[0][1]]; 
             const mirroredPoint2 = [plotCoordinates[0][0], plotCoordinates[1][1]];
             const boundingBoxCoordinates = plotCoordinates.slice();
@@ -126,9 +136,9 @@ class Map extends Component {
                     [boundingBoxCoordinates],
                     'boundingBoxLayer',
                     'fill',
-                    {   'fill-color':'#ddd' }
+                    {'fill-color':'#3366ff', 'fill-opacity': .5, 'fill-outline-color': '#002080' }
                     );
-                }); 
+                });  
             map.off('click', this.setCoordinates);
          }
      }       
@@ -172,20 +182,70 @@ class Map extends Component {
         });
     }
 
+     infoText = {
+         Circle: 'Click on a point to set the center, then click on another point to set the radius. Click on the Circle Tool button again to complete.',
+         BoundingBox: 'Click two points to set the corner edges of the bounding box and then click the Polygon tool again to close the shape.',
+         Polygon: 'Click to set polygon points and then click the Polygon Tool again to close the shape.',
+         null: 'Click on a tool to set the drawing mode.'
+     }
+
    render() {
+       const { type, boundingBoxCoordinates, circleCoordinates, polygonCoordinates } = this.state;
         return (
-            <>
-                <div className='map-style' id='map' />
-                <div className='drawing-controls'>
-                    <button onClick={this.clickHandler} id="Point">Circle</button>
-                    <button onClick={this.clickHandler} id="Polygon">Polygon</button>
-                    <button onClick={this.clickHandler} id="boundingBox">Bounding Box</button>
-                    <button onClick={this.closePolygon} id="closePolygon">Close Polygon</button>
-                    <button onClick={this.closeCircle} id="closeCircle">Close Circle</button>
-                    <button onClick={this.closeBoundingBox} id="closeBoundingBox">Close Bounding Box</button>
-                    <button onClick={this.clear} id="clear">Clear</button>
+            <div className='wrapper'>
+                <div className='header'></div>
+                <div className='main'>
+                    <div className='grid'>                
+                    <div className='map-style' id='map' />
+
+                    <div className="tools">
+                    <h4 className="tools-heading">Tools:</h4>
+                        <div className='drawing-controls'>                            
+                            <button onClick={this.clickHandler} id="Circle"><span><img src={circleIcon} alt="circle-icon"/></span></button>
+                        
+                            <button onClick={this.clickHandler} id="Polygon"><span><img src={polyIcon} alt="polygon-icon"/></span></button>
+                            
+                            <button onClick={this.clickHandler} id="BoundingBox"><span><img src={boxIcon} alt="boxIcon"/></span></button>
+                            
+                            <button onClick={this.clear} id="clear"><span><img src={trashIcon} alt="clear-icon"/></span></button>
+                        </div>
+                    </div>
+                    <div className='info'>                        
+                        <div className='info-box'>                        
+                            <h5 className="info-heading">Drawing Mode: </h5>
+                              <p className="info-text"><code>{type}</code></p>      
+                        </div>
+                        <div className="info-box">
+                            <h5 className="info-heading">Info:</h5>
+                            <p className="info-text"><code >{this.infoText[type]}</code></p>
+                        </div>
+                        <div className='info-box'>
+                        <h5 className="info-heading">Coordinates:</h5>
+
+                            { circleCoordinates  ?
+                            <div className="info-text">
+                                <p>{JSON.stringify({circleCoordinates}, null,  4)}</p>  
+                            </div>       
+                            : null}
+
+                            { boundingBoxCoordinates  ?
+                            <div className="info-text">
+                                <p>{JSON.stringify({boundingBoxCoordinates}, null,  4)}</p>  
+                            </div>       
+                            : null}
+
+                            { polygonCoordinates  ?
+                            <div className="info-text">
+                                <p>{JSON.stringify({polygonCoordinates}, null,  4)}</p>  
+                            </div>       
+                            : null}
+
+                        </div>
+                        
+                    </div>
+                    </div>
                 </div>
-            </>
+            </div>
         );
     }
 }
