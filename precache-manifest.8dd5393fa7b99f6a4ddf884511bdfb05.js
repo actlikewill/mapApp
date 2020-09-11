6a2815b68fd21a97cf434a4498dc107db5b58abe
actlikewill <wilsongaturu@gmail.com>
2020-09-11 20:14:56 +0300

    Updates

---
+++ precache-manifest.8dd5393fa7b99f6a4ddf884511bdfb05.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42a7ada617ce6cf6383348e74f14f17",
+    "url": "/mapApp/index.html"
+  },
+  {
+    "revision": "a411740cd1a3e5f8e24d",
+    "url": "/mapApp/static/css/2.75edc52f.chunk.css"
+  },
+  {
+    "revision": "04ddf25e88bc85c3cb6c",
+    "url": "/mapApp/static/css/main.49e9d838.chunk.css"
+  },
+  {
+    "revision": "a411740cd1a3e5f8e24d",
+    "url": "/mapApp/static/js/2.1ec29dc5.chunk.js"
+  },
+  {
+    "revision": "04ddf25e88bc85c3cb6c",
+    "url": "/mapApp/static/js/main.a574130d.chunk.js"
+  },
+  {
+    "revision": "75ade88f126ed19b3c35",
+    "url": "/mapApp/static/js/runtime-main.25be7640.js"
+  },
+  {
+    "revision": "638e008adeee89643c60e5944cdc43e6",
+    "url": "/mapApp/static/media/box.638e008a.svg"
+  },
+  {
+    "revision": "1e2d05951acbb42885df43fe821f1d0b",
+    "url": "/mapApp/static/media/circle.1e2d0595.svg"
+  },
+  {
+    "revision": "24ad147d68084ca4b8872844ce38d5fa",
+    "url": "/mapApp/static/media/garbage.24ad147d.svg"
+  },
+  {
+    "revision": "df01d317cad09b2c3653c0db30818b01",
+    "url": "/mapApp/static/media/polygon.df01d317.svg"
+  }
+]);
